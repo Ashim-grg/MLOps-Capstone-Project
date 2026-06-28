0835a491c409d6bbe07cb0407d5c988bc748ec1b
Raj Gurung <razzgrg56@gmail.com>
2026-06-28 19:20:04 +0545

    Project structure ready ✅

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an end-to-end Capstone Project using all the components of MLOps.',
+    author='Raj',
+    license='',
+)
